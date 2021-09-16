81122bd5d9c8327008224acbefab2296f6bac9ac
SentDyem <55694826+SentDyem@users.noreply.github.com>
2021-09-16 10:11:40 +0300

    new changes

---
+++ pulya-ar/Assets/vb_anim.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vb_anim : MonoBehaviour
+{
+    GameObject btn;
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        btn = GameObject.Find("VirtualButton");
+        anim = GetComponentInChildren<Animator>();
+        btn.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(btnPressed);
+        btn.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(btnReleased);
+    }
+
+    private void btnReleased(VirtualButtonBehaviour obj)
+    {
+        anim.Play("Stop");
+    }
+
+    private void btnPressed(VirtualButtonBehaviour obj)
+    {
+        anim.Play("Take 001");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
