7400adb9f197f1ba3c846b0a90265cbfcd2c1097
SentDyem <55694826+SentDyem@users.noreply.github.com>
2021-09-18 22:57:17 +0300

    new

--- pulya-ar/Assets/vb_anim.cs
+++ pulya-ar/Assets/vb_anim.cs
@@ -1,35 +1,50 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using Vuforia;
 
 public class vb_anim : MonoBehaviour
 {
-    GameObject btn;
+    public VirtualButtonBehaviour vb;
     public Animator anim;
+    public GameObject info;
+    bool pressed = false;
     // Start is called before the first frame update
     void Start()
     {
-        btn = GameObject.Find("VirtualButton");
+        // btn = GameObject.Find("VirtualButton");
         anim = GetComponentInChildren<Animator>();
-        btn.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(btnPressed);
-        btn.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(btnReleased);
+        vb.RegisterOnButtonPressed(btnPressed);
+        vb.RegisterOnButtonReleased(btnReleased);
     }
 
     private void btnReleased(VirtualButtonBehaviour obj)
     {
-        anim.Play("Stop");
+
+        info.SetActive(true);
+        
     }
 
     private void btnPressed(VirtualButtonBehaviour obj)
     {
-        anim.Play("Take 001");
+        if (pressed == false)
+        {
+            anim = GetComponentInChildren<Animator>();
+            anim.Play("Take 001");
+            info.SetActive(false);
+            pressed = true;
+        }
+        else
+        {
+            info.SetActive(false);
+            // pressed = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
